d7a085a1853979804737c6821577986a1518090f
Pedro Paradella <pparadella@gmail.com>
2018-09-07 19:02:35 -0300

    added information about me

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,7 +2,8 @@
 var bio = {
   "name" : "Pedro Henrique Alvim Paradella",
   "role" : "Web Developer",
-  "skills" : ["Html5", "Css3", "Javascript", "C", "C#", "GameMaker", "SQL"],
+  "skills" : ["Html5", "Css3", "Javascript", "SQL", "Python", "JQuery","JSLink", "C", "C#", "VisualStudio 2018", "GameMaker", "Git", "GitHub",
+  "Tortoise","Montagem e Manutenção de Computadores", "Redes", "Dreamweaver"," Atom", "Adobe Muse", "Adobe Photoshop", "Adobe Flash", "Pacote Office", "CorelDraw", "GIMP"],
   "biopic" : "./img/bioPic.jpg"
 };
 
@@ -14,6 +15,20 @@ var education = {
       "desc" : "Bacharelado em Ciência da Computação (Cursando)",
       "dates" : "Previsão 2019.1",
       "url" : "https://uva.br/"
+    },
+    {
+      "name" : "Udacity",
+      "location" : "Online",
+      "desc" : "Desenvolvedor Web Full Stack Nanodegree (Cursando)",
+      "dates" : "2019",
+      "url" : "https://br.udacity.com/course/full-stack-web-developer-nanodegree--nd004"
+    },
+    {
+      "name" : "Udacity",
+      "location" : "Online",
+      "desc" : "Desenvolvedor Web Front-End Nanodegree (Cursando)",
+      "dates" : "2018",
+      "url" : "https://br.udacity.com/nanodegree"
     },
     {
       "name" : "Col. Est. Pedro Álvares Cabral ",
@@ -28,6 +43,13 @@ var education = {
       "desc" : "Ensino Medio Técnico em informática.",
       "dates" : "2006",
       "url" : "http://www.liceudearteseoficios.com.br/"
+    },
+    {
+      "name" : "Advanced InfoTech School",
+      "location" : "Copacabana, Rio de Janeiro",
+      "desc" : "Técnico em Informática.",
+      "dates" : "2005",
+      "url" : ""
     }
   ]
 };
@@ -35,32 +57,39 @@ var education = {
 var work = {
   "jobs" : [
     {
+      "employer" : "Metatron",
+      "title" : "Estagiário de Desenvolvimento",
+      "location" : "Centro, Rio de Janeiro, RJ",
+      "dates" : "2018",
+      "desc" : "Utilizo javascript, jquery, jslink, css, C# e outras API's para criar produtos novos e melhorar a experiência do usuário com o sharepoint."
+    },
+    {
       "employer" : "Freelancer",
-      "title" : "Front-end Developer",
+      "title" : "Desenvolvedor Front-end",
       "location" : "Rio de Janeiro, RJ",
-      "dates" : "2007-2017",
-      "desc" : "Develop websites."
+      "dates" : "2007-2018",
+      "desc" : "Desenvolvi websites/portifólios responsivos."
     },
     {
       "employer" : "Protomni Multimedia",
       "title" : "Game Developer",
       "location" : "Rio de Janeiro, RJ",
-      "dates" : "2012-2017",
-      "desc" : "Implement everything a game requires, create Sprites(specialized in 8-bit generation) and 3D models."
+      "dates" : "2012-2018",
+      "desc" : "Implementei tudo que um jogo precisa, desde a criação de sprites(especializado em pixel-art) até a codificação do jogo."
     },
     {
       "employer" : "Golden Tulip Hotel",
-      "title" : "IT Trainee",
+      "title" : "Estágio em TI",
       "location" : "Copacabana, Rio de Janeiro, RJ",
       "dates" : "2010",
-      "desc" : "Support for employees and employers, machine repair, private network repair and servers."
+      "desc" : "Dei suporte de TI para os funcionários e os hospedes do hotel."
     },
     {
       "employer" : "Ocean Air Linhas Aéreas",
       "title" : "Airline Agent",
       "location" : "Praça XV, Rio de Janeiro, RJ",
       "dates" : "2009-2010",
-      "desc" : "Check-in of passengers, sell tickets, boarding/unboarding airplanes and their supervision."
+      "desc" : "Fiz check-in de passageiros, embarque e desembarque nas aeronaves e sua devida supervisão."
     }
   ]
 };
@@ -68,34 +97,67 @@ var work = {
 var projects = {
   "projects" : [
     {
+      "title" : "Currículo Online",
+      "dates" : "2018",
+      "description" : "Meu currículo online feito para mostrar minhas habilidades e meu portifólio.",
+      "images" : ["./img/curriculo.png"],
+      "url" : ["https://pparadella.github.io/new-resume/","https://github.com/pparadella/new-resume"]
+    },
+    {
+      "title" : "Sql log-analyzer",
+      "dates" : "2018",
+      "description" : "Uma ferramenta de relatórios que imprime relatórios (em texto sem formatação) com base nos dados do banco de dados. Esta ferramenta de relatórios é um programa de Python usando o módulo psycopg2 para se conectar ao banco de dados. Feito para o nanodegree Udacity Web full-stack.",
+      "images" : [""],
+      "url" : ["https://github.com/pparadella/sql-log-analyzer"]
+    },
+    {
+      "title" : "Site feito em python",
+      "dates" : "2018",
+      "description" : "Um site gerado por código python feito para o nanodegree Udacity Web full-stack.",
+      "images" : [""],
+      "url" : ["https://github.com/pparadella/MovieSitePython"]
+    },
+    {
+      "title" : "Meu site responsivo",
+      "dates" : "2017",
+      "description" : "Um simples e responsivo site feito para praticar.",
+      "images" : ["./img/website.png"],
+      "url" : ["https://pparadella.github.io/", "https://github.com/pparadella/pparadella.github.io"]
+    },
+    {
+      "title" : "Arcade Game",
+      "dates" : "2017",
+      "description" : "Jogo feito para o nanodegree Udacity Front-end em html5 usando canvas e javascript.",
+      "images" : ["./img/arcade.png"],
+      "url" : ["https://pparadella.github.io/frontend-nanodegree-arcade-game/", "https://github.com/pparadella/frontend-nanodegree-arcade-game"]
+    },
+    {
+      "title" : "Web site responsivo",
+      "dates" : "2017",
+      "description" : "Web site criado para o nanodegree Udacity Front-end.",
+      "images" : ["./img/webresponsive.png"],
+      "url" : ["https://pparadella.github.io/responsive-project/", "https://github.com/pparadella/responsive-project"]
+    },
+    {
       "title" : "Bernie Needs Love",
       "dates" : "2015",
       "description" : "A retro platformer game about an old man who collects blue pills amidst urban dangers and bird poop in a desperate effort to reach his girlfriend before he is himself reached by the ever-approaching Grim Reaper.",
-      "images" : ["./img/bernie1.jpg"]
+      "images" : ["./img/bernie1.jpg"],
+      "url" : ["https://store.steampowered.com/app/373120/Bernie_Needs_Love/"]
     },
     {
       "title" : "Meatballphobia",
       "dates" : "2013",
       "description" : "Meatballphobia is a 2D physics puzzle game about flesh and meat, about the enslavement of both the consumed and the consumer, about life and the systems built around it. It is also a game about making a meatball hit an unfortunate hysterical head.",
-      "images" : ["./img/meatball1.jpg"]
-    },
-    {
-      "title" : "SindiBebRj",
-      "dates" : "2017",
-      "description" : "Website for a beverage transportation union.",
-      "images" : [""]
-    },
-    {
-      "title" : "Prime",
-      "dates" : "2014",
-      "description" : "Website for a company.",
-      "images" : [""]
+      "images" : ["./img/meatball1.jpg"],
+      "url" : ["http://www.protomnimultimedia.com/meatballphobia.html"]
     },
     {
       "title" : "Villa Carpe Diem",
       "dates" : "2007",
-      "description" : "Website for a hostel.",
-      "images" : [""]
+      "description" : "Web site elaborado em flash para uma pousada.",
+      "images" : [""],
+      "url" : ["https://pparadella.github.io/sites/villa/in.htm"]
     }
   ]
 };
@@ -161,7 +223,16 @@ projects.display = function () {
 
     //display image
     for (var image in projects.projects[each].images){
-      $('.projects-item .item-desc').eq(each).append('<br><img src="'+ projects.projects[each].images[image] +'">');
+      if (projects.projects[each].images[image] != ""){
+          $('.projects-item .item-desc').eq(each).append('<br><img src="'+ projects.projects[each].images[image] +'">');
+      }
+    }
+    $('.projects-item .item-desc').eq(each).append('<br>');
+    //display url
+    for (var url in projects.projects[each].url){
+      if (projects.projects[each].url[url] != ""){
+        $('.projects-item .item-desc').eq(each).append('<br><a target="_blank" href="'+ projects.projects[each].url[url] +'">'+ projects.projects[each].url[url] +'</a><br>');
+      }
     }
   }
 };
